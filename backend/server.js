6324c0d1cba7c280fc51cb0eeec02b3850184121
Nguyen Thanh Hieu <thanhhieu@192.168.1.18.non-exists.ptr.local>
2024-11-17 12:18:43 +0700

    done

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+import app from './app.js';
+
+// Tải các biến môi trường từ file .env
+dotenv.config();
+
+const PORT = process.env.PORT || 5001;  // Port mặc định là 5000 nếu không có trong .env
+
+// Khởi động server
+app.listen(PORT, () => {
+  console.log(`Server started at http://localhost:${PORT}`);
+});
